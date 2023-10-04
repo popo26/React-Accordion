222908e68d46ae92d534320a3467285af4b9b353
popo26 <mynameispopo26@gmail.com>
2023-10-05 08:36:29 +1300

    media query experiments - portrait and landscape

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const triviaResults = async (term) => {
-    const response = await axios.get("https://opentdb.com/api.php?amount=3");
+    const response = await axios.get("https://opentdb.com/api.php?amount=3&type=boolean");
     
     return response.data.results;
 }
